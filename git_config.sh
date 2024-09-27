253f65e737e6e0717d222b662b44376cd6ae4310
jimkocky <monniebush@gmail.com>
2024-09-27 08:36:10 +0200

    Welcome

---
+++ git_config.sh
@@ -0,0 +1,6 @@
+#!bin/dash
+echo "Welcome to Git config"
+git config --global user.email "dolezalondrej.007@gmail.com"
+git config --global user.name "jimkocky"
+git config --list
+echo "Done successfully"
